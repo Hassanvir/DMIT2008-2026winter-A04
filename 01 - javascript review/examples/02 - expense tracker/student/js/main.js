a38e253cc95a2a41546bc5bd242f3b17a013552f
Hassanvir Singh <hassanvirsingh5@gmail.com>
2026-01-21 10:01:12 -0700

    Update main.js

--- 01 - javascript review/examples/02 - expense tracker/student/js/main.js
+++ 01 - javascript review/examples/02 - expense tracker/student/js/main.js
@@ -1,1 +1,30 @@
 //Instruction On expense-tracker-code-explained.md
+import theExpenses from "./expense-data.js";
+
+const expenseContainer = document.getElementById("expense-container");
+
+function renderExpenses(expenses) {
+  const expenseList = document.getElementById("expense-list");
+  expenseContainer.innerHTML = "";
+
+    expenses.forEach((expense) => {
+        
+    expenseContainer.innerHTML += `
+      <div class="card" id="${expense.id}">
+        <div class="header">
+          <div>
+            <div class="title">${expense.title}</div>
+            <div class="meta category">${expense.category}</div>
+          </div>
+          <div class="amount">${expense.amount}</div>
+        </div>
+        <div class="meta date">${expense.date}</div>
+        <div class="actions">
+          <button class="edit-btn" id=${expense.id}>Edit</button>
+          <button class="delete-btn" id=${expense.id}>Delete</button>
+        </div>
+      </div>`;
+  });
+}
+
+renderExpenses(theExpenses);
